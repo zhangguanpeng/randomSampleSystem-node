ac5a7c6a1a57abbf0001b9befdcfdcdfee28cc47
zhangguanpeng <zguanpeng@126.com>
2017-04-13 19:04:00 +0800

    shibai

--- js/domAction.js
+++ js/domAction.js
@@ -3,11 +3,15 @@ $(document).ready(function() {
 	//设置内容区域高度
 	$(".main-container").height(window_height - 55);
 	//设置表格区域高度
-	$("#dataContent").height(window_height - 145);
-	$("#dataTable_div").height(window_height - 180);
-	//$("#resultTable").height(window_height - 145);
-	var marquee;
+	$("#dataContent").height(window_height - 155);
+	$("#resultContent").height(window_height - 155);
+	$("#dataTable_div").height(window_height - 190);
+	$("#resultTable_div").height(window_height - 190);
+	var sampleData = [];  //定义样本数据
+	var marquee;  //定义动画对象
+	/*点击左侧不同菜单触发执行不同的流程*/
 	$(".nav-list .submenu").on('click', 'a', function(){
+		$("#sampleNumber").val(''); //抽取表单致空
 		var clickOption = $(this).text();
 		switch (clickOption) {
 			case "文艺演出":
@@ -23,21 +27,32 @@ $(document).ready(function() {
 				alert("其他");
 		}
 	});
+	/*点击‘开始抽取’按钮触发*/
+	$("#btn_getRandomData").on('click', function() {
+		var randomNumber = $("#sampleNumber").val();
+		//alert(randomNumber);
+		var resultData = getArrayItems(sampleData, randomNumber);
+		//console.log(resultData);
+		//setInterval("console.log('haha')",1000);
+		//var int=setInterval("initResultTable(resultData)",1000)
+		initResultTable(resultData);
+	});
 
 	function changeTitle(newTitle) {
 		$(".home-title").text(newTitle);
 	}
-
+	/*更新数据表格人数*/
 	function updateTotalNumber(totalNumber) {
 		var newTotalNumber = "专家名单（共" + totalNumber + "人）";
 		$("#dataTitle").text(newTotalNumber);
 	}
-
+	/*获取数据*/
 	function getData(dataSourceName) {
 		$.ajax({
 			url: 'http://127.0.0.1:8081/getdata',
 			type: 'GET',
 			success: function(data) {
+				sampleData = data;
 				initTable(data);
 			},
 			error: function(XMLHttpRequest, textStatus, errorThrown){
@@ -48,7 +63,7 @@ $(document).ready(function() {
 		    },
 		});
 	}
-
+	/*创建数据源表格*/
 	function initTable(data) {
 		$("#dataTable").empty();
 		var tbBody = "";
@@ -67,6 +82,92 @@ $(document).ready(function() {
 			return;
 		}
 		marquee = new Marquee({ ID: "dataTable_div", Direction: "top", Step: 2, Width: 0, Height: 500, Timer: 50, DelayTime: 0, WaitTime: 0, ScrollStep: 80  });
+	}
+	/*随机抽取函数*/
+	function getArrayItems(arr, num) {
+	    //新建一个数组,将传入的数组复制过来,用于运算,而不要直接操作传入的数组;
+	    var temp_array = new Array();
+	    for (var index in arr) {
+	        temp_array.push(arr[index]);
+	    }
+	    //取出的数值项,保存在此数组
+	    var return_array = new Array();
+	    for (var i = 0; i<num; i++) {
+	        //判断如果数组还有可以取出的元素,以防下标越界
+	        if (temp_array.length>0) {
+	            //在数组中产生一个随机索引
+	            var arrIndex = Math.floor(Math.random()*temp_array.length);
+	            //将此随机索引的对应的数组元素值复制出来
+	            return_array[i] = temp_array[arrIndex];
+	            //然后删掉此索引的数组元素,这时候temp_array变为新的数组
+	            temp_array.splice(arrIndex, 1);
+	        } else {
+	            //数组中数据项取完后,退出循环,比如数组本来只有10项,但要求取出20项.
+	            break;
+	        }
+	    }
+	    return return_array;
+	}
+	/*创建随机抽取结果表格*/
+	function initResultTable(data) {
+		$("#resultTable").empty();
+		var tbBody = "";
+		var trColor = "";
+		/*var trColor;
+		var i=0;
+		while (i<data.length) {
+			if(i == 0) {
+				trColor = "even-tr";
+				tbBody = "<tr id='tr" +i + "' class='" + trColor + "'><td width='10%'>" + data[i].id + "</td>" + "<td width='10%'>" + data[i].name + "</td>" + "<td width='30%'>" + data[i].department + "</td>" + "<td width='30%'>" + data[i].specialty + "</td>" + "<td width='20%'>" + data[i].jobTitle + "</td></tr>";
+				setTimeout(addTr1, 1000);
+				i++;
+			} else{
+				if (i % 2 == 0) {
+		            trColor = "even-tr";
+		        }
+		        else {
+		            trColor = "odd-tr";
+		        }
+		        tbBody = "<tr id='tr" +i + "' class='" + trColor + "'><td width='10%'>" + data[i].id + "</td>" + "<td width='10%'>" + data[i].name + "</td>" + "<td width='30%'>" + data[i].department + "</td>" + "<td width='30%'>" + data[i].specialty + "</td>" + "<td width='20%'>" + data[i].jobTitle + "</td></tr>";
+				setTimeout(addTr2, 1000);
+				i++;
+			}
+		}*/
+		
+		for(var i=0;i<data.length;i++) {
+			(function(i) {
+		        if (i % 2 == 0) {
+		            trColor = "even-tr";
+		        }
+		        else {
+		            trColor = "odd-tr";
+		        }
+		        tbBody = "<tr id='tr" +i + "' class='" + trColor + "'><td width='10%'>" + data[i].id + "</td>" + "<td width='10%'>" + data[i].name + "</td>" + "<td width='30%'>" + data[i].department + "</td>" + "<td width='30%'>" + data[i].specialty + "</td>" + "<td width='20%'>" + data[i].jobTitle + "</td></tr>";
+	        	if(i == 0) {
+	        		setTimeout(function() {
+	        			$('#resultTable').append(tbBody);
+	        			//alert("i=0");
+	        			console.log($('#resultTable').html());
+	        		}, 1000);
+	        	}else {
+	        		setTimeout(function() {
+	        			$("#tr" + i-1).after(tbBody);
+	        			//alert("i!=0");
+	        			console.log($('#resultTable').html());
+	        		}, 1000);
+	        	}
+	  
+			})(i);
+		}
+		
+		/*function addTr1() {
+			debugger;
+			$('#resultTable').append(tbBody);
+		}
+		function addTr2() {
+			debugger;
+			$("#tr" + i-1).after(tbBody);
+		}*/
 	}
 
 });
